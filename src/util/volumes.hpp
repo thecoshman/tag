fc11ae85e7c894086c43a8a9f1666014b72b2c31
thecoshman <thecoshman@gmail.com>
2015-03-06 20:58:15 +0000

    Got distracted with templates
    AABB is now fully templated (tm)

--- src/util/volumes.hpp
+++ src/util/volumes.hpp
@@ -1,13 +1,20 @@
+#pragma once
+
 #include <glm/glm.hpp>
 
 namespace util{
-struct AABB{
-    AABB(glm::vec3 midPoint, double width, double height, double depth): AABB(midPoint.x, midPoint.y, midPoint.z, width, height, depth) {}
-    AABB(double x, double y, double z, double width, double height, double depth):
+
+template <typename V>
+struct AxisAlignedBoundingBox{
+    using T = typename V::value_type;
+
+    AxisAlignedBoundingBox(V midPoint, T width, T height, T depth): AxisAlignedBoundingBox(midPoint.x, midPoint.y, midPoint.z, width, height, depth) {}
+    AxisAlignedBoundingBox(T x, T y, T z, T width, T height, T depth):
         min(x - (width / 2), y - (height / 2), z - (depth / 2)),
         max(x + (width / 2), y + (height / 2), z + (depth / 2)) {}
 
-    glm::vec3 min;
-    glm::vec3 max;
+    V min, max;
 };
+
+typedef AxisAlignedBoundingBox<glm::vec3> AABB;
 }
